02118f9c7c63ff747ff230155fcb0cdc39ff8eed
drennout <serega10lad@gmail.com>
2022-12-29 12:41:20 +0300

    add admin-app

---
+++ admin-app/src/test/java/ru/serzhe1/adminapp/AdminAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.serzhe1.adminapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
